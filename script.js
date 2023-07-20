a27314f587bfa2f7a17b26218f86af4a8baeacb4
MR-Sharingan <94732487+MR-Sharingan@users.noreply.github.com>
2023-07-20 23:02:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// console.log(document.querySelector('.message').textContent);
+document.querySelector('.check').addEventListener('click', function (){
+    const ans = (document.querySelector(".guess").value);
+    // console.log(ans);
+});
